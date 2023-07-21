3e65dbedac4edeb45fef43e21532be7bf9e96d0e
diemich <diemich29@gmail.com>
2023-07-21 15:32:58 +1200

    second commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+#The terraform {} block is required so Terraform knows which provider to download from the Terraform Registry.
+# In the configuration above, the google provider's source is defined as hashicorp/google which is shorthand 
+#for registry.terraform.io/hashicorp/google.
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+# GCP provider
+provider "google" {
+  project      = var.project_id
+  region       = var.gcp_region
+  zone         = var.gcp_zone
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+    default     = "n_project"
+}
+variable "gcp_region" {
+    default     = "n_region"
+}
+variable "gcp_zone" {
+    default     = "n_zone"
+}
