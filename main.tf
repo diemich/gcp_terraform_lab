e0155c985cd8d4082b29cc8465b5f349020ca497
diemich <diemich29@gmail.com>
2023-07-22 01:29:42 +1200

    final adjustments

--- main.tf
+++ main.tf
@@ -25,4 +25,6 @@ module "instances" {
 module "storage" {
   source = "./modules/storage"
 }
-
+#module "firewall" {
+#source = "./modules/networking"
+#}
--- shell_variable.sh
+++ shell_variable.sh
@@ -25,10 +25,14 @@ sed -i "s/n_region/$n_region/g" "$path_file"
 sed -i "s/n_zone/$n_zone/g" "$path_file"
 sed -i "s/n_bucket/$n_bucket/g" "$path_file"
 
+#sed -i "s/n_project/$n_project/g" firewall.tf
+#sed -i "s/n_vpc/$n_vpc/g" firewall.tf"
+
 #copy variables to instances and storage folder
 cp ~/gcp_terraform_lab/variables.tf ~/gcp_terraform_lab/storage/
 cp ~/gcp_terraform_lab/variables.tf ~/gcp_terraform_lab/instances/
+cp ~/gcp_terraform_lab/variables.tf ~/gcp_terraform_lab/networking/
 
 #import modules 
-terraform import module.instances.google_compute_instance.tf-instance-1 $tf_i1
-terraform import module.instances.google_compute_instance.tf-instance-2 $tf_i2
+#terraform import module.instances.google_compute_instance.tf-instance-1 $tf_i1
+#terraform import module.instances.google_compute_instance.tf-instance-2 $tf_i2
